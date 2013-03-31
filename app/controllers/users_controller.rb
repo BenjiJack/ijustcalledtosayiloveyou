2ffc8582bbeddf81108c8b23849e630bafc12219
Benjamin Jack <benjaminjack@gmail.com>
2013-03-31 13:54:04 -0400

    Phone number validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,15 +23,13 @@ require 'twilio-ruby'
 	@call = @client.account.calls.create(
   		:from => "+12677764203",
   		:to => @user[:recipientphone],
+  		# returns XML using twimlets - see https://www.twilio.com/labs/twimlets/echo
   		#<Response>
 		#<Say voice="woman">Hi X, Y has a message for you!</Say>
 		#<Play>http://agile-beach-2909.herokuapp.com/ijustcalled.mp3</Play>
 		#<Say voice="woman">I just called to say I love you dot com</Say>
 		#</Response>
-  		:url => "http://twimlets.com/echo?Twiml=%3CResponse%3E%0A%3CSay%20voice%3D%22woman%22%3EHi%20" + @user[:recipientname] + "%2C%20" + @user[:sendername] + "%20has%20a%20message%20for%20you!%3C%2FSay%3E%0A%3CPlay%3Ehttp%3A%2F%2Fagile-beach-2909.herokuapp.com%2Fijustcalled.mp3%3C%2FPlay%3E%0A%3CSay%20voice%3D%22woman%22%3EI%20just%20called%20to%20say%20I%20love%20you%20dot%20com%3C%2FSay%3E%0A%3C%2FResponse%3E&"
-# returns XML using twimlets - see https://www.twilio.com/labs/twimlets/echo
-#<Response><Play>http://agile-beach-2909.herokuapp.com/ijustcalled.mp3</Play></Response>
-  		#:url => "http://twimlets.com/echo?Twiml=%3CResponse%3E%3CPlay%3Ehttp%3A%2F%2Fagile-beach-2909.herokuapp.com%2Fijustcalled.mp3%3C%2FPlay%3E%3C%2FResponse%3E&"
+  		:url => "http://twimlets.com/echo?Twiml=%3CResponse%3E%0A%3CSay%20voice%3D%22woman%22%3EHi%20" + @user[:recipientname].split.join("%20") + "%2C%20" + @user[:sendername].split.join("%20") + "%20has%20a%20message%20for%20you!%3C%2FSay%3E%0A%3CPlay%3Ehttp%3A%2F%2Fagile-beach-2909.herokuapp.com%2Fijustcalled.mp3%3C%2FPlay%3E%0A%3CSay%20voice%3D%22woman%22%3EI%20just%20called%20to%20say%20I%20love%20you%20dot%20com%3C%2FSay%3E%0A%3C%2FResponse%3E&"
 	)
 
 	#Send explanatory text message
@@ -46,7 +44,7 @@ require 'twilio-ruby'
 
 
   	else
-  		flash[:failure] = "Not sent"
+  		flash[:failure] = "Complete all fields. Use <25 char and valid phone #"
   		redirect_to root_path
   	end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+
+require 'validates_phone_number'
+
   attr_accessible :recipientname, :recipientphone, :sendername
 
   validates :sendername, presence: true, length: { maximum: 25 }
   validates :recipientname, presence: true, length: { maximum: 25 }
 
-  validates :recipientphone, presence: true
-
+  validates :recipientphone, presence: true, :phone_number => { :format => /\+1\s\(\d{3}\)\s\d{3}\-\d{4}/ }
 
 end
