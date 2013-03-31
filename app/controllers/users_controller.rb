9d78a101755dbb3bc50ea2c0020ddf8d6fd034bc
Benjamin Jack <benjaminjack@gmail.com>
2013-03-30 21:45:26 -0400

    Functioning site playing dummy mp3 and sending txt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,26 @@ require 'twilio-ruby'
 
   	if @user.save
 
+  	#Instantiate Twilio client
 	account_sid = 'AC31e65c7b5abd8ea30ade9abbbd169abf'
 	auth_token = 'ab3f73cd65470a3fc1ae817d74a56bbd'
 	@client = Twilio::REST::Client.new account_sid, auth_token
 
-		@client.account.sms.messages.create(
-  			:from => "+12677764203",
-  			:to => "+1#{@user[:recipientphone]}",
-  			:body => "#{@user[:sendername]} just called to say I love you!"
-		)
+	#Make phone call and play sound file
+	@call = @client.account.calls.create(
+  		:from => "+12677764203",
+  		:to => "+1#{@user[:recipientphone]}",
+  		# returns XML using twimlets - see https://www.twilio.com/labs/twimlets/echo
+	#<Response><Play>http://demo.twilio.com/hellomonkey/monkey.mp3</Play></Response>
+		:url => 'http://twimlets.com/echo?Twiml=%3CResponse%3E%3CPlay%3Ehttp%3A%2F%2Fdemo.twilio.com%2Fhellomonkey%2Fmonkey.mp3%3C%2FPlay%3E%3C%2FResponse%3E&'
+	)
+
+	#Send explanatory text message
+	@client.account.sms.messages.create(
+  		:from => "+12677764203",
+  		:to => "+1#{@user[:recipientphone]}",
+  		:body => "#{@user[:sendername]} just called to say I love you!"
+	)
 
   		flash[:success] = "Sent!"
   		redirect_to root_path
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ Ijustcalledtosayiloveyou::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
 end
