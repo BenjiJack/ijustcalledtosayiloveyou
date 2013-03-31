c17de3e692ff00e60b6ae8f7b7c5ceb1cf9cabcc
Benjamin Jack <benjaminjack@gmail.com>
2013-03-30 20:23:46 -0400

    Working text message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+require 'twilio-ruby'
+
+
+
+
   def new
   	@user = User.new
   end
@@ -8,6 +13,16 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
 
   	if @user.save
+
+	account_sid = 'AC31e65c7b5abd8ea30ade9abbbd169abf'
+	auth_token = 'ab3f73cd65470a3fc1ae817d74a56bbd'
+	@client = Twilio::REST::Client.new account_sid, auth_token
+
+		@client.account.sms.messages.create(
+  			:from => "+12677764203",
+  			:to => "+1#{@user[:recipientphone]}",
+  			:body => "#{@user[:sendername]} just called to say I love you!"
+		)
 
   		flash[:success] = "Sent!"
   		redirect_to root_path
